036df1f8fbb20275ce67539175242bbac8909e0f
AshleyvanderVelden <33858983+AshleyvanderVelden@users.noreply.github.com>
2017-11-28 10:10:51 +0100

    mappen structuur aangepast
    mappen structuur aangepast en twee scripts toegevoegt

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    public Vector3 vecturTwee
+        ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vecturTwee.x = -Input.GetAxis("Mouse Y");
+        transform.Rotate(vecturTwee);
+    }
+}
---
+++ Assets/Scripts/TestRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRotate : MonoBehaviour {
+    public Vector3 r;
+    public float hor;
+    public float ver;
+    public Vector3 v;
+    public int ms;
+    public Vector3 vecturTwee;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        vecturTwee.y = Input.GetAxis("Mouse X");
+
+        transform.Rotate(vecturTwee);
+
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        v.x = hor;
+        v.z = ver;
+
+        transform.Translate(v/ms);
+	}
+}
